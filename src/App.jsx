be2e1571c954534fcab57d31eca36b7d96f551fd
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-01-21 17:01:33 +0330

    merge line with develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./assets/styles/App.css"
 import Nav from "./components/Navbar/Nav";
 import Header from "./components/Header/Header"
+import { LineH } from "./components/separatorLine/Line";
+
 const App = () => {
   return (
     <div className="container">
       <Nav/>
       <Header/>
+      <LineH/>
     </div>
   );
 };
---
+++ src/components/separatorLine/Line.jsx
@@ -0,0 +1,11 @@
+import "../../assets/styles/line.css"
+
+export const LineH = ()=>{
+    return(
+        <div className="line-container-H">
+            <div className="line"></div>
+            <p>وبلاگ های بروز شده</p>
+            <div className="line"></div>
+        </div>
+    )
+}
